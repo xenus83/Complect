5037cb8a003ef4b13fc62a17d28c8fe853ba997c
Xenus83 <xenus83@yandex.ru>
2023-12-27 02:57:58 +0300

    Currnet_state_27_12_2023_0257

--- wp-content/plugins/LFManagement/LFManagement.php
+++ wp-content/plugins/LFManagement/LFManagement.php
@@ -163,7 +163,8 @@ class LFManagement
 					$ff = 1;							
 					//TODO сделать какую-то обработку... дополнительно
 				}
-				
+				elseif(!isset($arr[$def_key]) && $def_val != "_obligatory")
+					$arr[$def_key] = $def_val
 			}
 		}
 
